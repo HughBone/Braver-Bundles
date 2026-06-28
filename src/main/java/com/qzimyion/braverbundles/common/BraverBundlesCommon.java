ee92b2234f2c721ba4f6f7ab0827e98e1d3d2815
HughBone <hughbone27@gmail.com>
2026-06-28 11:35:34 -0400

    update to 26.2

--- src/main/java/com/qzimyion/braverbundles/common/BraverBundlesCommon.java
+++ src/main/java/com/qzimyion/braverbundles/common/BraverBundlesCommon.java
@@ -11,21 +11,6 @@ public final class BraverBundlesCommon {
     public static void init() {
         MidnightConfig.init(BraverBundlesConstants.MOD_ID, CommonModConfig.class);
         DispenserBlock.registerBehavior(Items.BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.WHITE_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.ORANGE_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.CYAN_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.BLUE_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.BROWN_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.BLACK_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.GRAY_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.GREEN_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.LIGHT_BLUE_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.LIGHT_GRAY_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.LIME_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.MAGENTA_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.PINK_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.PURPLE_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.RED_BUNDLE, new BundleDispenserBehavior());
-        DispenserBlock.registerBehavior(Items.YELLOW_BUNDLE, new BundleDispenserBehavior());
+        Items.DYED_BUNDLE.forEach(bundle -> DispenserBlock.registerBehavior(bundle, new BundleDispenserBehavior()));
     }
 }
